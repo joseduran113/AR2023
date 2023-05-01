cc410b6682d0fae5f0d3a603f53f46de7dc38d25
Jose <jose.duran.gonzalez@alumnos.upm.es>
2023-05-02 01:14:27 +0200

    commit

---
+++ Assets/botonprueba1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class botonprueba1 : MonoBehaviour
+{
+public GameObject canvas;
+
+public void accion()
+{
+    canvas.SetActive(false);
+}
+}
